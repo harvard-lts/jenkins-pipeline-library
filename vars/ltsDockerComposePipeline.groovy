b1b9e4c3561178f5783339699014a3f41a053550
Jessi Jassal <jessica_jassal@harvard.edu>
2022-09-30 16:21:30 -0400

    Simple pipeline

---
+++ vars/ltsDockerComposePipeline.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy
+
+def call(List imageNames, String stackName, String projName, String slackChannel = "lts-jenkins-notifications", String intTestPort, List intTestEndpoints) {
+    pipeline {
+      agent any
+      stages {
+        stage('Configure') {
+          when { anyOf { branch 'main'; branch 'trial' } }
+          steps {
+            script {
+              config_script()
+            }
+          }
+        }
+        environment {
+            imageNames = []
+            stackName = ''
+            // projName is the directory name for the project on the servers for it's docker/config files
+            projName = ''
+            slackChannel=''
+            registryCredentialsId = "${env.REGISTRY_ID}"
+            registryUri = 'https://registry.lts.harvard.edu'
+        }
+      }
+    }
+}
+
+def config_script(){
+    GIT_TAG = sh(returnStdout: true, script: "git tag | head -1").trim()
+    echo "$GIT_TAG"
+    GIT_HASH = sh(returnStdout: true, script: "git rev-parse --short HEAD").trim()
+    echo "$GIT_HASH"
+}
